8ddadcdc85feaf0235ac8497afb8a9be4f2562d7
chuck21619 <charles.ward.johnston@gmail.com>
2024-08-19 13:58:15 -0400

    finish chapter 2:Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
@@ -11,13 +12,31 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0
 	
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	
+	Player.containers = (updatable, drawable)
+	
+	x = SCREEN_WIDTH/2
+	y = SCREEN_HEIGHT/2
+	
+	player = Player(x, y)
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		
+		something = clock.tick(60)
+		dt = something/1000
+		
+				
 		screen.fill("black")
+		for item in updatable:
+			item.update(dt)
+		for item in drawable:
+			item.draw(screen)
 		pygame.display.flip()
-		dt = clock.tick(60)/1000.0
 
 if __name__ == "__main__":
 	main()
